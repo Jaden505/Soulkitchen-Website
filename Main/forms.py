d3d3ce9c2ca7af5982469b50947fe6595a4dddb3
Jaden <jadenxxxl@gmail.com>
2020-06-18 10:52:53 +0200

    Change menu page and hotbar better for phone

--- Main/forms.py
+++ Main/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 
 class UserInfo(forms.Form):
-    email = forms.EmailField(label='Email')
-    address = forms.CharField(label='Adres')
-    city = forms.CharField(label='Stad')
-    zip_code = forms.CharField(label='Post code')
+    email = forms.EmailField(label='Email', max_length=100)
+    phone = forms.RegexField(regex=r'^\+?1?\d{9,15}$')
+    address = forms.CharField(label='Adres', max_length=100)
+    city = forms.CharField(label='Stad', max_length=100)
+    zip_code = forms.CharField(label='Post code', max_length=100)
 
