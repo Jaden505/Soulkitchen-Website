d74834c8b9760d556a6cc868cf44fa1b2bcf3c06
Jaden <jadenxxxl@gmail.com>
2020-07-15 23:52:08 +0200

    Second aplha commit. Added vegan and spicy displays. Chang banner to logo.

--- Web/settings.py
+++ Web/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 import mimetypes
-import re
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sassies-soulkitchen.herokuapp.com']
 
